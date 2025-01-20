8f4031e1ec4bbfec6bf65db2f57216fa00a115f3
Prerana Nawar <61601706+prerana1821@users.noreply.github.com>
2025-01-20 16:26:51 +0000

    testing

--- basics.cpp
+++ basics.cpp
@@ -1,7 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Running";
-    return 0
+int main() {
+  int x, y;
+  cin >> x >> y;
+  cout << "Prerana" << endl << "Nawar" << "\n";
+  cout << "values are: " << x << " " << y;
+  return 0;
 }
